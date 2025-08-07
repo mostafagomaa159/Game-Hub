ae4a418253a6a79253b9368f2078581c6031895c
unknown <mos.gom.17@gmail.com>
2025-08-07 06:39:06 +0300

    aaaaa

--- Game-Hub-Frontend/src/components/adminTabs/DisputesTab.js
+++ Game-Hub-Frontend/src/components/adminTabs/DisputesTab.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState, useCallback } from "react";
-import axios from "axios";
+import axios from "../../api/axiosInstance";
 
 const DisputesTab = () => {
   const [disputes, setDisputes] = useState([]);
